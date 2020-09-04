dc814db35ee37c02b15dfd73ed370183899a4f67
Skyler Cornell <skylerhcornell@gmail.com>
2020-09-04 09:18:34 -0400

    integrated spotify API into web page

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
 from flask import Flask, request, render_template, redirect, url_for, flash, jsonify
 from SpotifyAPI import SpotifyAPI
-#pp = pprint.PrettyPrinter(indent=2)
+import pprint
+
+pp = pprint.PrettyPrinter(indent=2)
 CLIENT_ID = '9008c46566394946badacf432c783c08'
 CLIENT_SECRET = '85c80468c4234b0bb05db88ea53aed0c'
 
 app = Flask(__name__)
 
+spotify = SpotifyAPI(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+
 @app.route('/form', methods=['POST'])
 def form():
-    print('IN HERE FROM POST')
-    print(request.form)
-    return redirect(url_for('home'))
+    # get user entered key words from form
+    entered_text = request.form['keywords-single-str']
+    keywords_list = entered_text.split(',')
+
+    # do spotify stuff with the keuwords
+    spotify.auth()
+    playlists = spotify.find_playlists(keywords_list)['playlists']['items']
+    playlist_names = []
+    playlist_owners = []
+    playlist_descriptions = []
+
+    for playlist in playlists:
+        playlist_names.append(playlist['name'])
+        playlist_owners.append(playlist['owner'])
+        playlist_descriptions.append(playlist['description'])
+    return render_template('form.html', playlist_names=playlist_names, playlist_owners=playlist_owners, playlist_descriptions=playlist_descriptions)
+
 
 @app.route('/')
 def home():
